4964a83e42058ba79ef8d86351c2fc3902974015
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2023-09-12 11:00:11 +0500

    Project built into one file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Tkinter calculator by Mikhail Zhizhikin, 2023.
+This application is the simple calculator made by me using the Python and its Tkinter library.
+I made it to learn the Tkinter.
+"""
+
 from tkinter import *
 from tkinter import ttk
 
@@ -15,11 +21,11 @@ style.theme_use('alt')
 style.configure('TButton', background = 'black', foreground = 'white', font='Arial 15', width = 20, borderwidth=0, focusthickness=0, focuscolor='none')
 style.map('TButton', background=[('active', '#888')])
 
-# Configuring the grid to place widgets beautifully
+# Configuring the grid to place widgets beautifully.
 for c in range(5): root.columnconfigure(index=c, weight=1)
 for r in range(5): root.rowconfigure(index=r, weight=1)
 
-# To see evaluation that we write
+# To see evaluation that we write.
 entry_text = StringVar()
 entry_field = Entry(font=('Consolas 30'), bg='black', fg='white', borderwidth=0, justify=CENTER, textvariable=entry_text)
 entry_field.grid(row=0, column=0, columnspan=5, sticky=NSEW)
@@ -28,7 +34,7 @@ entry_field.grid(row=0, column=0, columnspan=5, sticky=NSEW)
 def equals_button_command():
 	output_entry_text = '...'
 	try:
-		# Solve evaluation in text field to output
+		# Solve evaluation in text field to output.
 		output_entry_text = eval(entry_field.get())
 	except ZeroDivisionError:
 		output_entry_text = 'Division by zero.'
@@ -36,21 +42,21 @@ def equals_button_command():
 		output_entry_text = 'Result is too large.'
 	except (ValueError, SyntaxError, NameError):
 		output_entry_text = 'Enter an evaluation.'
-	# Output result in text field
+	# Output result in text field.
 	entry_text.set(output_entry_text)
 
 
-# To add characters to text field using buttons
+# To add characters to text field using buttons.
 def add_character_to_entry_field(character: str):
 	entry_field.insert(END, character)
 
 
-# To place button widgets in window
+# To place button widgets in window.
 def add_buttons_to_window(column_count, index):
 	# We substract 5 from index, because first 5 columns of grid is used by text field.
 	button_text = ALL_BUTTONS_TEXT[index - 5]
 	text_button_command = None
-	# If index position in text list equals 4 ('C' button) we clear screen
+	# If index position in text list equals 4 ('C' button) we clear screen.
 	if index == len(ALL_BUTTONS_TEXT) + 4:
 		text_button_command = lambda: entry_text.set('')
 	# Or index button is '=', we solve the evaluation
